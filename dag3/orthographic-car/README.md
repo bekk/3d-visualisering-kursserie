1c4b935930b8ece6e9d8290401eabf52090b135a
Holger Ludvigsen <holger.ludvigsen@bekk.no>
2018-10-29 14:25:37 +0100

    Legger til oppgavetekst of utdelt kode for orthographic-car

--- dag3/orthographic-car/README.md
+++ dag3/orthographic-car/README.md
@@ -1,6 +1,6 @@
 # Orthographic car
 
-I denne oppgaven lager vi et lite bilspill i en orthografisk verden. Det er ingen shaderprogrammering i denne oppgaven!
+I denne oppgaven lager vi et lite bilspill i en orthografisk verden. Det er ingen shaderprogrammering i denne oppgaven! I stedet for å få forklart hver eneste kodelinje går vi gjennom hva som må gjøres, og hvilke komponenter og triks du må bruke for å få det til.
 
 ![Resultat Orthographic Car](./img/orthographic-car.gif)
 
@@ -22,36 +22,82 @@ og fasiten kan kjøres med:
 npm run fasit-orthographic-car
 ```
 
-## TODO
+## What to do
 
-TODO
+Den utdelte koden setter opp et `OrthographicCamera` som ser på en bilformet boks. Et orthografisk kamera har ikke perspektiv på samme måte som vanlige kameraer (og som vårt eget øye). I stedet for at alle linjer samles mot horisonten (tenk perspektiv-tegningene fra ungdomsskolens kunst og håndverk) er alle linjer parallelle (tenk gamle rollespill på Nintendo). I denne orthografiske verden skal vi lage et bilspill!
 
-- Lag bakke med 5x5 BoxGeometry
-- Lag bil med BoxGeometry og og CylinderGeometry
+### 1. Lag en bakke ved hjelp av 5x5 `BoxGeometry` og et fint materiale 
 
-- Styr bilen med piltastene
-    + car.getWorldDirection(direction)
-    + car.rotation.y
+Tips: For å oppnå god ytelse er det lurt å gjenbruke en geometry-instans og en material-instans om de skal være like.
 
-- Flytt kamera like langt som bilen har flyttet seg fra startposisjonen slik at det følger etter hele tiden
+Tips: For din egen del bør du strukturere koden litt. Lag f.eks. en ny funksjon `makeGround()`
 
-- Roter hjulene på bilen når den kjører
-- Fall nedover og game over når man kjører ut av brettet
-    + new THREE.Box3().setFromObject()
-    + carBBox.intersectsBox(groundBBox)
+### 2. Legg til fire hjul på bilen.
 
-- Lag poeng-kuler på tilfeldig plassering med SphereGeometry
-- Lag hindre med på tilfeldig plassering med ConeGeometry
+I den utdelte koden er det en enkel boks som representerer bilen. Men den er rimelig døll.
 
-- Lag skygge med 
-    + renderer.shadowMap
-    + shadowLight.shadow
-    + .receiveShadow
-    + .castShadow
+Tips: `THREE.CylinderGeometry` blir et fint hjul
 
-- Sjekk kollisjon med alle poeng og hindre
-    + Fjern poeng-kuler når man tar poeng
-    + Flytt bilen i motsatt retning av hinderet når man kolliderer med det
+Tips: Samle objekter i `new THREE.Group()` for å kunne flytte de sammen.
 
-Ytelsestips:
-- Gjenbruk samme Geometry- og Material-instans
+### 3. Beveg bilen med piltastene.
+
+Tips: I den utdelte `util.js` er det en funksjon `addKeyListeners` som tar et objekt som parameter og legger til lyttere for tastetrykk. Objektet får verdien 0 eller 1 for hver av piltastene: `{left: 0, right: 0, up: 0, down: 0}`.
+
+Tips: For å få en vektor som peker i bilens retning kan man bruke `car.getWorldDirection(direction)`. Send inn en `Vector3` som parameter, og den vil populeres med retningen. (Å sende inn en vektor i stedet for å returnere en ny vektor er en vanlig praksis i 3D-bibliotek for å unngå å måtte opprette for mange nye objekter.)
+
+Tips: Endre på `.position` og `.rotation.y` for å henholdsvis forflytte og rotere rundt y-aksen.
+
+### 4. Flytt kamera like mye som bilen har flyttet seg
+
+Nå risikerer vi å kjøre bilen utenfor kameraet. Det er jo ikke slik bilspill funker, er det vel? Nei, vi må flytte kamera slik at det følger etter bilen.
+
+Tips: I den utdelte koden er det en funksjon `moveCar()` som er tenkt for dette.
+
+### 5. Roter hjulene på bilen når den kjører
+
+Bilen ser jo ikke helt realistisk ut, og det er viktig å gi spilleren følelsen av fart og mekanikk.
+
+Tips: Det kan være ganske vanskelig å se at en perfekt sylinder roterer. Men i dokumentasjonen til CylinderGeometry ser vi at vi kan sette `radialSegments` ganske lav for å kunne se fasettene: https://threejs.org/docs/#api/en/geometries/CylinderGeometry
+
+### 6. Fall ned når bilen er utenfor brettet
+
+Hvis du har prøvd å kjøre utenfor brettet har du sett at immersion brytes ganske hardt når du når kanten. Og immersion er viktig i spill.
+
+Tips: Kollisjonshåndtering er et eget fagfelt i 3D. Men en simpel måte er å bruke `THREE.js` sin innebygde `Box3`. Det er en axis-aligned boks i rommet med størrelse og plassering.
+
+Tips: Om man har et objekt i scenen kan man lage en "bounding box" rundt dette med `new THREE.Box3().setFromObject(mittObjekt)`. En bounding box er en boks som er akkurat så stor som den trenger å være for å dekke hele objektet.
+
+Tips: Et objekt MÅ være lagt til scenen før man kan beregne en bounding box. Ellers vet man jo ikke plasseringen og størrelsen i scenen.
+
+Tips: Har man en `Box3` ved navn `boks1` kan man lett sjekke om den overlapper (kolliderer) med en annen boks: `boks1.intersectsBox(boks2)`.
+
+### 7. Lag poeng-kuler på tilfeldige plasseringer
+
+Et godt spill har et mål, og i dette spillet blir målet å samle inn alle poeng-kulene. (Eller poeng-tetrahedronene? Du bestemmer.)
+
+Tips: Sjekk om bilen kolliderer med et poeng ved hjelp av `Box3.instersectsBox()`
+
+Tips: Når man har tatt et poeng kan det skjules ved å endre feltet `.visible` til `false`. (Alle `Object3D` har dette feltet.)
+
+### 8. Lag hindre som man kolliderer med
+
+Et spennende spill har også utfordringer.
+
+Tips: Vil du f.eks. ha små pyramider som hindre kan man bruke `ConeGeometry`
+
+Tips: Når man kolliderer med et hinder kan bilen simpelthen sprette tilbake i den retningen den kom fra. Flytt bilen et lite stykke i retningen mellom hinderets og bilens posisjoner.
+
+### 9. Pimp that shit
+
+Spillet vårt er ikke helt på Unity-nivå når det gjelder grafikk og gameplay. Men noen enkle triks kan gjøres. For eksempel:
+
+- Få poengene til å eksplodere når de tas
+
+- Lag en mer detaljert bil med vinduer, hjulrom og terrengdekk
+
+- Animer bilene videre slik at den steiler når man akselererer og lener seg ut i svingene
+
+- Legg på skygge på bakken med `DirectionalLightShadow`: https://threejs.org/docs/#api/en/lights/shadows/DirectionalLightShadow
+
+- Lag et hopp hvor bilen løftes opp lineært på vei opp rampen
--- dag3/orthographic-car/index.js
+++ dag3/orthographic-car/index.js
@@ -2,11 +2,7 @@ const THREE = require('three');
 const fs = require('fs');
 const util = require('./util.js');
 
-let camera, renderer, scene, car, collisionObstacle, ground, points, obstacles, hasWon, hasLost, gravity = 0;
-const collisionHelpers = [];
-let pointsTaken = 0;
-
-const keyPressed = {};
+let camera, renderer, scene, car;
 
 const initialCameraPosition = new THREE.Vector3(-100, 100, -100);
 
@@ -15,9 +11,6 @@ function initAnimation() {
     renderer.setClearColor(0xA0D9FE);
     renderer.setSize(window.innerWidth, window.innerHeight, true);
     renderer.setPixelRatio(window.devicePixelRatio || 1);
-    
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap; 
 
     const ratio = renderer.getContext().drawingBufferWidth / renderer.getContext().drawingBufferHeight;
 
@@ -31,257 +24,32 @@ function initAnimation() {
 
     document.body.appendChild(renderer.domElement);
 
-    util.addKeyListeners(keyPressed);
-
-    ground = makeGround();
-    scene.add(ground);
-
-    obstacles = makeObstacles();
-    scene.add(obstacles);
-
     car = makeCar();
     scene.add(car);
 
-    points = makePoints()
-    scene.add(points)
-
-    makeCollisionHelpers();
-
-    const light = new THREE.DirectionalLight(0xffffff, 0.8);
+    const light = new THREE.DirectionalLight(0xffffff, 1.0);
     light.position.set(-1, 2.5, -2);
     scene.add(light);
-
-    const light2 = new THREE.DirectionalLight(0xffffff, 0.2);
-    light2.position.set(0, 1, 1);
-    scene.add(light2);
-
-    scene.add(new THREE.AmbientLight(0xffffff, 0.5));
-
-    const shadowLight = new THREE.DirectionalLight(0x0, 0.0);
-    shadowLight.position.set(0, 20, 0)
-    shadowLight.castShadow = true;
-    scene.add(shadowLight);
-
-    shadowLight.shadow.camera.right    =  50;
-    shadowLight.shadow.camera.left     = -50;
-    shadowLight.shadow.camera.top      =  50;
-    shadowLight.shadow.camera.bottom   = -50;
 }
 
 function makeCar() {
-    const car = new THREE.Group();
-
-    const body = new THREE.Mesh(
+    const car = new THREE.Mesh(
         new THREE.BoxGeometry(2, 2, 4), 
         new THREE.MeshLambertMaterial({color: 0xd1d1d1})
     )
-
-    const wheelGeometry = new THREE.CylinderGeometry(1, 1, 1, 8);
-    wheelGeometry.rotateZ(Math.PI/2);
-    wheelGeometry.computeFlatVertexNormals();
-
-    const wheels = [];
-
-    for (let x = -1; x <= 1; x+=2) {
-        for (let y = -1; y <= 1; y+=2) {
-            const wheel = new THREE.Mesh(
-                wheelGeometry, 
-                new THREE.MeshLambertMaterial({color: 0xd1d1d1, flatShading: true})
-            );
-
-            wheel.position.set(x*1.5, 1, y * 1.5);
-            wheel.castShadow = true;
-            car.add(wheel);
-            wheels.push(wheel);
-        }
-    }
-
-    body.position.y = 1.5;
-    body.castShadow = true;
-
-    car.add(body);
-
-    car.castShadow = true;
-    car.wheels = wheels;
-
     return car;
 }
 
-function makeGround() {
-    const ground = new THREE.Group();
-
-    for (let x = -2; x <= 2; x++) {
-        for (let y = -2; y <= 2; y++) {
-            const tile = new THREE.Mesh(
-                new THREE.BoxGeometry(20-0.1, 1, 20-0.1), 
-                new THREE.MeshLambertMaterial({color: 0x808080})
-            );
-
-            tile.position.set(x * 20, -1/2, y * 20);
-            tile.receiveShadow = true;
-            ground.add(tile);
-        }
-    }
-
-
-    return ground;
-}
-
-function makePoints() {
-    const points = new THREE.Group();
-
-    const pointGeometry = new THREE.SphereGeometry(1, 16, 32);
-    const pointMaterial = new THREE.MeshLambertMaterial({color: 0xd11d1d});
-
-    for (let x = 0; x < 5; x++) {
-        const point = new THREE.Mesh(
-            pointGeometry, 
-            pointMaterial
-        );
-
-        point.position.set(util.random(-2*20, 2*20), 1/2 + 1, util.random(-2*20, 2*20));
-        point.castShadow = true;
-        points.add(point)
-    }
-
-
-    return points;
-}
-
-function makeObstacles() {
-    const obstacles = new THREE.Group();
-
-    const obstacleGeometry = new THREE.ConeGeometry(5, 10, 4);
-    obstacleGeometry.computeFlatVertexNormals();
-    obstacleGeometry.rotateY(Math.PI/4);
-
-    const obstacleMaterial = new THREE.MeshLambertMaterial({color: 0x3060c0});
-
-    for (let x = 0; x < 5; x++) {
-        const obstacle = new THREE.Mesh(
-            obstacleGeometry, 
-            obstacleMaterial
-        );
-
-        obstacle.position.set(util.random(-2*20, 2*20), 1/2 + 1, util.random(-2*20, 2*20));
-        obstacles.add(obstacle)
-    }
-
-
-    return obstacles;
-}
-
-function makeCollisionHelpers() {
-    for (const point of points.children) {
-        collisionHelpers.push({
-            bbox: new THREE.Box3().setFromObject(point),
-            mesh: point,
-            type: "point"
-        })   
-    }
-
-    for (const obstacle of obstacles.children) {
-        collisionHelpers.push({
-            bbox: new THREE.Box3().setFromObject(obstacle),
-            mesh: obstacle,
-            type: "obstacle"
-        })   
-    }
-}
-
 function moveCar() {
-    const direction = new THREE.Vector3();
-    car.getWorldDirection(direction);
-
-    const speed = 0.5;
-    const rotationSpeed = 0.05;
-
-    if (collisionObstacle) {
-        const obstaclePosition = new THREE.Vector3();
-        collisionObstacle.getWorldPosition(obstaclePosition)
-        const diff = obstaclePosition.sub(car.position);
-        diff.y = 0;
-        
-        const collisionForce = -0.1;
-
-        car.position.add(diff.multiplyScalar(collisionForce));
-    }
-
-    if (keyPressed.up) {
-        car.position.add(direction.multiplyScalar(speed));
-
-        for (const wheel of car.wheels) {
-            wheel.rotation.x += 0.2;
-        }
-    }
-
-    if (keyPressed.down) {
-        car.position.sub(direction.multiplyScalar(speed));
-
-        for (const wheel of car.wheels) {
-            wheel.rotation.x -= 0.2;
-        }
-    }
-
-    if (keyPressed.left) {
-        car.rotation.y += rotationSpeed;
-    }
-
-    if (keyPressed.right) {
-        car.rotation.y -= rotationSpeed;
-    }
-
-    car.position.y -= gravity;
+    
 }
 
 function moveCamera() {
-    const carOffset = new THREE.Vector3(car.position.x, 0, car.position.z);
-    const cameraTarget = initialCameraPosition.clone().add(carOffset);
-    const cameraDiff = cameraTarget.clone().sub(camera.position);
-
-    const dampingFactor = 0.1;
-
-    camera.position.add(cameraDiff.multiplyScalar(dampingFactor))
+    
 }
 
 function checkIntersections() {
-    var carBBox = new THREE.Box3().setFromObject(car);
-    carBBox.min.y = 0;
-
-    collisionObstacle = false;
-
-    for (const collisionHelper of collisionHelpers) {
-        if (collisionHelper.bbox.intersectsBox(carBBox)) {
-            if (collisionHelper.type == "point") { 
-                if (collisionHelper.mesh.visible) {
-                    console.log("POINT");
-                    collisionHelper.mesh.visible = false;
-                    pointsTaken++;
-                }
-            } else if (collisionHelper.type == "obstacle") {
-                collisionObstacle = collisionHelper.mesh;
-            }
-        }
-    }
-
-    if (pointsTaken == points.children.length && !hasWon) {
-        console.log("GAME WON")
-        hasWon = true;
-    }
-
-    var groundBBox = new THREE.Box3().setFromObject(ground);
     
-    if (!carBBox.intersectsBox(groundBBox)) {
-        gravity = 1;
-    } else {
-        gravity = 0;
-    }
-
-    if (car.position.y < -10 && !hasLost) {
-        console.log("GAME LOST");
-
-        hasLost = true;
-    }
 }
 
 function animate() {
