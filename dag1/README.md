d57fdd1785ee0710442e0197d491b890b0c43f38
petteas <petter.astrup@bekk.no>
2018-11-14 22:02:13 +0100

    Make bonus task for day one task 3

--- dag1/README.md
+++ dag1/README.md
@@ -599,3 +599,59 @@ console.log(soundLevel);
 ```
 
 Skaler etterpå tallet i koden med å dele det for å få den ned på rimelige verdier. Resultatet skal være en visualisering rimelig lik bildet i starten av oppgaven.
+
+### Bonus: Gjør mer morsomme ting med lyd (Bør gjøres etter du har gjort dag 2)
+
+I denne oppgaven finnes det flere ting vi kan leke oss med. Her skal vi gjøre følgende for å få resultatet i bildet over:
+1. Få rødheten til lydsøylene til å avhenge av høyden på lydsøylen
+2. Få lydsøylene til å rotere
+
+For å få rødheten til lydsøylene til å avhenge av høyden på lydsøylen må vi kaste ut shaderen og bestemme fargen til søylene i Javascripten. 
+
+Kaste ut shader og erstatte materialet med BasicMeshMaterial:
+```diff
+- const fragmentShaderCode = fs.readFileSync(
+-   __dirname + "/fragmentshader.glsl",
+-   "utf8"
+- );
+...
+- const UNIFORMS = {
+-   soundLevel: { value: 0.0 }
+- };
+...
+-  const material = new THREE.ShaderMaterial({
+-    uniforms: UNIFORMS,
+-    fragmentShader: fragmentShaderCode
+-  });
++ const material = new THREE.MeshBasicMaterial( { color: 0xff0000} )); //rød
+...
+-  let soundLevel = 0;
+-  for (let i = 0; i < frequencies.length; i++) {
+-    soundLevel = soundLevel += frequencies[i];
+-  }
+-  UNIFORMS.soundLevel.value = soundLevel;
+```
+Når det nå er borte kan vi begynne å bli kreative med søylene. For hver søyle setter vi som kjent høyden til hver kube med følgende loop:
+```
+  cubes.forEach((c, i) => {
+    const freq = normalise(min, max, frequencies[i]);
+    c.scale.set(1, freq, 1);
+  });
+```
+
+Her inne har vi en freq-variabel vi scaler kubene med. Bruk den samme variabelen til å regne ut en hexadesimal fargekode for rød som blir rødere jo høyere frekvensen er. Sett så fargen med følgende kode:
+```
+  c.material.color.setHex( colorhex );
+```
+
+Hint: en farge-hex består av en rød, en gul og en blå del. det er kun den røde delen du trenger å bry deg om her. 0xff0000 er helt rød og 0x000000 er selvfølgelig helt fargeløs.
+
+Gjør nå det samme for rotasjonen til kubene om x-aksen. Vi vil oppnå større rotasjon ved en høyere freq-verdi. Man kan rotere kubene med følgende innebygde funksjon. Husk at vinkelen er i radianer
+
+```
+c.rotateX(angle);
+```
+
+Til slutt kan du parametrisere det hele ved hjelp av dat.gui. forslag til parametre: 
+- Fart på rotasjon
+- Baserødhet
---
+++ dag1/fasit/oppgave3Bonus/index.js
@@ -0,0 +1,105 @@
+const THREE = require("three");
+const OrbitControls = require("three-orbit-controls")(THREE);
+const analyse = require("./soundanalyser.js");
+const fs = require("fs");
+const dat = require('dat.gui');
+
+let scene, camera, renderer, cubes, analyser, soundLevel;
+
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+
+const NUM_CUBES = 32;
+
+const params = {
+  baseColor: "#ff9500",
+  speed: 50.0
+}
+
+let texture = new THREE.Color( params.baseColor );
+
+function init() {
+
+
+  scene = new THREE.Scene();
+  scene.background = texture
+  initCubes();
+  initCamera();
+  initRenderer();
+  initDatGui();
+  document.body.appendChild(renderer.domElement);
+
+  renderer.render(scene, camera);
+}
+
+function initDatGui() {
+  var gui = new dat.GUI();
+
+  gui.addColor(params, 'baseColor');
+  gui.add(params, "speed", 0.0, 100.0);
+}
+
+
+function initCamera() {
+  camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 1000);
+  camera.position.z = 40;
+
+  new OrbitControls(camera);
+}
+
+function initRenderer() {
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(WIDTH, HEIGHT);
+}
+
+function initCubes() {
+  cubes = Array(NUM_CUBES)
+    .fill()
+    .map(function(_, i) {
+      let n = -1 * Math.floor(NUM_CUBES / 2) + i;
+      let cube = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1), new THREE.MeshBasicMaterial( { color: 0xff0000} ));
+      cube.position.set(n * 1 + n * 0.1, 0, 0);
+      scene.add(cube);
+      return cube;
+    });
+}
+
+function normalise(min, max, v) {
+  return ((v - min) / max) * -1;
+}
+
+function updateParameters(){
+  scene.background = new THREE.Color( params.baseColor );
+}
+
+function makeCubesDance() {
+  let min = analyser.analyser.minDecibels;
+  let max = analyser.analyser.maxDecibels;
+  let frequencies = analyser.frequencies();
+  cubes.forEach((c, i) => {
+    const freq = normalise(min, max, frequencies[i]);
+    c.scale.set(1, freq, 1);
+    const freqDamperFactor = 10;
+    const factor = Math.min(Math.max((freq)/freqDamperFactor, normalise(min, max, 0) / freqDamperFactor), 1);
+    const redhex = Math.floor(0xff * factor);
+    const redstring = redhex.toString(16);
+    const colorhexstring = redstring + "0000";
+    const colorhex = parseInt(colorhexstring, 16);
+    c.material.color.setHex(colorhex);
+    const rotateAngle = factor <= normalise(min, max, 0)/freqDamperFactor ? 0 : factor*(Math.PI/32);
+    c.rotateX(rotateAngle * params.speed/freqDamperFactor);
+  });
+}
+
+function render() {
+  requestAnimationFrame(render);
+  makeCubesDance();
+  updateParameters();
+  renderer.render(scene, camera);
+}
+
+init();
+analyse(function(a) {
+  analyser = a;
+  render();
+});
